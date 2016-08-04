a24cd5a12158eae3d8eaf3e15cf399d9a6f06738
Yonathan Ayenew <yonayen@gmail.com>
2016-08-04 15:52:02 -0700

    add get function to server

--- hello_server.js
+++ hello_server.js
@@ -6,4 +6,24 @@ app.get('/', function(request, response) {
     response.send("Hello World");
 });
 
-app.listen(process.env.PORT, process.env.IP);
+app.get('/:firstname/:lastname', function(request, response) {
+    var first = request.params.firstname;
+    var last = request.params.lastname;
+    response.send(["Hello", first, last].join(" "));
+});
+/// slash is important
+app.get('/jedi/:firstname/:lastname', function(request, response) {
+    var first = request.params.firstname;
+    var last = request.params.lastname;
+    // hints: splice()
+    response.send(["Hello ", 
+    	capitalize(last.slice(0,3)),
+    	capitalize(first.slice(0,2))]
+    	.join(""));
+});
+
+var capitalize = function (string) {
+	return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+app.listen(process.env.PORT||8080 );
